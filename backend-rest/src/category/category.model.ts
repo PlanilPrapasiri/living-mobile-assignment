ca234335d09edeb5218a8c6851967bccd78c700f
PlanilPrapasiri <prapasiri@livingmobile.me>
2021-06-23 00:57:19 +0700

    edit ForeignKey at category

--- backend-rest/src/category/category.model.ts
+++ backend-rest/src/category/category.model.ts
@@ -22,7 +22,7 @@ export class CategoryModel extends Model {
         type: DataType.UUID,
     })
     @ForeignKey(()=> StoreModel)
-    storeId: StoreModel;
+    storeId: string;
 
     // @HasMany(()=> MenuModel)
     // menuId : MenuModel[]
